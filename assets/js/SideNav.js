85644945e0e19af4b98e81c4313adfdd78bd59d5
Crafted-app1 <alexb270907@gmail.com>
2025-11-07 17:20:49 +0000

    Added the side bar to the home, log and profile pages

---
+++ assets/js/SideNav.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidebar").style.width = "125px";
+  document.getElementById("main").style.marginLeft = "125px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
